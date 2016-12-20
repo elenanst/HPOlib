6869dd118e2a66a0cb8f6d1597f3a2ea164b030d
Eleni Nisioti <elennisi@auth.gr>
2016-12-21 00:19:08 +0200

    calculated metafeatures of workspace

--- my_scripts/datasets/process_datasets.R
+++ my_scripts/datasets/process_datasets.R
@@ -1,12 +1,12 @@
  # define Class attribute in all dataset 
 rm(list=ls())
-setwd("/home/elena/R_ws/automl/ADS_workspace/datasets_repo/test")
+setwd("/home/elena/R_ws/automl/ADS_workspace/datasets_repo/configured")
 files_list = list.files(pattern="*.csv")
 files_list
 #files = lapply(files_list, function(x) read.csv(x, header = FALSE, sep=",", stringsAsFactors=FALSE))
 #files
-files = as.data.frame(read.csv(files_list[[1]], header = TRUE, sep=",", stringsAsFactors=FALSE))
-
+files = as.data.frame(read.csv(files_list[[45]], header = TRUE, sep=",", stringsAsFactors=FALSE))
+files<- as.data.frame(lapply(files, function(x) as.numeric(gsub("?",NA,x, fixed = TRUE))))
 files[files == "*"] <-0
 files_factor <- as.data.frame(lapply(files, factor))
 num_files_factor <- lapply(files_factor, nlevels)
